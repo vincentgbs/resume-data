452b401713776f486653ce4264d088b0334e3a47
Vincent H <vincentgbs@users.noreply.github.com>
2021-04-26 18:58:41 -0400

    Adding data JSON

---
+++ data.js
@@ -0,0 +1,193 @@
+"use strict";
+const resume = {
+    linkedin: 'https://www.linkedin.com/in/vincent-h-hu',
+    github: 'https://github.com/vincentgbs',
+    address: 'Doraville, Atlanta, GA 30341',
+    phone: '(404) 981-4727',
+    email: 'vincent.gbs11@gmail.com',
+    objective: "A computer science student with experience in financial analysis and web development, looking for a software development internship",
+    experience: [
+        {
+            position:'application development co-op',
+            location:'atlanta, ga',
+            company:'aflac',
+            description: [
+                'Updated .NET API codebase to enable automated deployment',
+            ],
+            start: 'August 2020',
+            end: 'December 2020'
+        },
+        {
+            position:'devops co-op',
+            location:'atlanta, ga',
+            company:'aflac',
+            description: [
+                'Implemented Newman for Postman in Build Pipelines to automate unit testing',
+                'Built and deployed a new CI/CD pipeline for the Messaging Process Service',
+            ],
+            start: 'January 2020',
+            end: 'August 2020'
+        },
+        {
+            position:'software analyst intern',
+            location:'atlanta, ga',
+            company:'manhattan associates',
+            description: [
+                'Created a tutorial for Java development using a Feign client and Eureka server',
+                'Wrote a Python script to validate database settings across VMs',
+            ],
+            start: 'September 2019',
+            end: 'December 2019'
+        },
+        {
+            position:'application development co-op',
+            location:'columbus, ga',
+            company:'aflac',
+            description: [
+                'Migrated hard coded system settings from Java to Mongo DB',
+                'Secured Finance Admin application by implementing backend authentication through Java Spring Boot',
+            ],
+            start: 'May 2019',
+            end: 'August 2019'
+        },
+        {
+            position:'senior financial manager; director of finance',
+            location:'peachtree corners, ga',
+            company:'the cresca group',
+            description: [
+                // 'Prepared and analyzed financial statements for clients',
+                // 'Managed data migration for new clients as they transition to our software service',
+                // 'Developed and supervised a summer internship program for the finance department'
+            ],
+            start: 'October 2014',
+            end: 'December 2019'
+        },
+        {
+            position:'financial analyst; senior financial manager',
+            location:'peachtree corners, ga',
+            company:'more of me to love',
+            description: [
+                // 'Created an accounting system for extracting data useful for sales analysis',
+                // 'Analyzed the financial state of the company and advised major business decisions',
+                // 'Worked on the sale of company assets and the resulting corporate restructuring'
+            ],
+            start: 'August 2012',
+            end: 'October 2014'
+        },
+        {
+            position:'software development intern',
+            location:'peachtree corners, ga',
+            company:'lamp camp',
+            description: [
+                'Developed a backend API for Amazon Merchant Services',
+                'Structured Mysql tables and queries for managing an automobile database'
+            ],
+            start: 'January 2014',
+            end: 'April 2014'
+        },
+    ],
+    education: [
+        {
+            school:'Georgia Institute of Technology',
+            degree:'Masters of Science',
+            majors: [
+                'Computer Science'
+            ],
+            description:[],
+            start: 'January 2020',
+            end: 'December 2023'
+        },
+        {
+            school:'Emory University - Goizueta Business School',
+            degree:'Bachelor of Business Administration',
+            majors: [
+                'Business<br><ul><li>Finance concentration</li><li>Accounting concentration</li></ul>',
+                'Economics'
+            ],
+            description:[
+                // 'Deans List Spring 2010, Fall 2010',
+                // 'Emory Scholar Scholarship'
+            ],
+            start: 'August 2007',
+            end: 'May 2011'
+        },
+    ],
+    skills: [
+        // 'apache',
+        'bitbucket',
+        'cpanel',
+        'css3-alt',
+        // 'excel',
+        'git',
+        'github-alt',
+        'html5',
+        'java',
+        'jira',
+        'js',
+        'linux',
+        // 'basic ruby development',
+        'php',
+        // 'basic python scripting',
+        'ubuntu'
+    ],
+    workflows: [
+        'Command-line Utility Automation',
+        'Mobile-First, Responsive Design',
+        'Agile Development & Scrum',
+    ],
+    volunteer: [
+        {
+            position:'senior php developer',
+            location:'atlanta, ga',
+            company:'journey church of atlanta',
+            description: [
+                'Designed and developed an MVC framework for backend needs',
+                'Built a customized CMS to meet pastors and church staff needs',
+                'Managed server administration and deployment on Dreamhost'
+            ],
+            start: 'January 2016',
+            end: 'August 2018'
+        },
+        {
+            position:'teaching assistant and tutor',
+            location:'clarkston, ga',
+            company:'refcode',
+            description: [
+                'Teaching front-end web development to students interested in computer science',
+                'Helping underprivileged refugees with computer literacy to improve their lives and job prospects'
+            ],
+            start: 'February 2019',
+            end: 'May 2019'
+        },
+        {
+            position:'other interests',
+            location:'Tech, Sports and Music',
+            company:'',
+            description: [
+                'Perpetual learning: I am always trying to figure out how things work: physically, mechanically, economically, fiscally, emotionally, spiritually, etc',
+                'Reading articles: I am passionate about technology, entreprenuership, and business and I try to stay up-to-date with these topics',
+                'Playing Sports: My favorite sports are basketball, volleyball, frisbee, football and pingpong',
+                'I also enjoy watching basketball and football. I am a NY Knicks fan <span class="list-inline-item"><i class="far fa-tired"></i></span>... yeah',
+                'I like to relax by playing the guitar or just hanging out with friends',
+            ],
+            start: '',
+            end: ''
+        },
+    ],
+    certifications: [
+        // 'Udemy: C# Developers: Double Your Coding Speed with Visual Studio - August 2020',
+        // 'Udemy: Learn DevOps: CI/CD with Jenkins using Piplines and Docker - August 2020',
+        // 'Udemy: Learn TDD in Java - August 2019',
+        // 'Udemy: Docker for the absolute beginner - August 2019',
+        // 'Udemy: CI/CD with Jenkins pipelines, Maven, Gradle - August 2019',
+        'Google IT Support Professional Certificate Specialization - March 2019',
+        'Quickbooks Online Certification - November 2018',
+        // 'Learning How to Learn (Coursera: McMaster University & University of California San Diego) - September 2018',
+        // 'High Performance Collaboration: Leadership, Teamwork, and Negotiation (Coursera: Northwestern University) - April 2016',
+        // 'Web Application Architectures (Coursera: University of New Mexico) with distinction - November 2014',
+        'Learn to Program: The Fundamentals (Coursera: University of Toronto) - January 2014',
+        // 'Internet History, Technology, and Security (Coursera: University of Michigan) with distinction - December 2013',
+        'NYS Insurance Certification: Broker Exam Series 10-51 - November 2011',
+        // '1<sup>st</sup>Place - Goizueta Business Essentials: Advanced Challenge - Spring 2010'
+    ],
+}
